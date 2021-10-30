ff6c8cb79502cf44613010c687ba3915ea584569
Ahmad Louis <ahmadlouis35@gmail.com>
2021-10-30 15:10:09 -0400

    arraylist feature for branch

--- drill1.java
+++ drill1.java
@@ -1,9 +1,13 @@
-
+import java.util.*;
+import java.io.*;
 
 public class drill1
 {
   public static void main(String[] args)
   {
-    System.out.println("Drill 1 for github maker");
+    System.out.println("Arraylist maker");
+    List <String> lister = new ArrayList<String>();
+
+    lister.add("Whats poppin");
   }
 }
